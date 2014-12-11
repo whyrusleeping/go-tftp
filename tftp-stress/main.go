983506c09729c5b7b4ce61faf20a09160dc9787b
Jeromy <jeromyj@gmail.com>
2014-12-11 08:15:04 +0000

    add option to reuse ports or not

--- tftp-stress/main.go
+++ tftp-stress/main.go
@@ -10,7 +10,7 @@ import (
 	"time"
 )
 
-func benchReads(server, file string, threads, loops, blocksize int) {
+func benchReads(server, file string, threads, loops, blocksize int, reuse bool) {
 	wg := &sync.WaitGroup{}
 
 	bwcollect := make(chan int, 32)
@@ -19,14 +19,24 @@ func benchReads(server, file string, threads, loops, blocksize int) {
 	for i := 0; i < threads; i++ {
 		wg.Add(1)
 		go func() {
+			var cli *client.TftpClient
 			defer wg.Done()
-			cli, err := client.NewTftpClient(server)
-			cli.Blocksize = blocksize
-			if err != nil {
-				panic(err)
+			if reuse {
+				cli, err := client.NewTftpClient(server)
+				cli.Blocksize = blocksize
+				if err != nil {
+					panic(err)
+				}
 			}
 
 			for j := 0; j < loops; j++ {
+				if !reuse {
+					cli, err := client.NewTftpClient(server)
+					cli.Blocksize = blocksize
+					if err != nil {
+						panic(err)
+					}
+				}
 				nbytes, _, err := cli.GetFile(file)
 				if err != nil {
 					panic(err)
@@ -57,7 +67,7 @@ func benchReads(server, file string, threads, loops, blocksize int) {
 	fmt.Printf("Overall Bandwidth: %.0f Bps\n", float64(sum)/took.Seconds())
 }
 
-func benchWrites(server string, threads, loops, nbytes, blocksize int) {
+func benchWrites(server string, threads, loops, nbytes, blocksize int, reuse bool) {
 	wg := &sync.WaitGroup{}
 
 	bwcollect := make(chan int, 32)
@@ -68,13 +78,25 @@ func benchWrites(server string, threads, loops, nbytes, blocksize int) {
 		wg.Add(1)
 		go func(thr int) {
 			defer wg.Done()
-			cli, err := client.NewTftpClient(server)
-			cli.Blocksize = blocksize
-			if err != nil {
-				panic(err)
+			var cli *client.TftpClient
+			var err error
+			if reuse {
+				cli, err = client.NewTftpClient(server)
+				cli.Blocksize = blocksize
+				if err != nil {
+					panic(err)
+				}
 			}
 
 			for j := 0; j < loops; j++ {
+				if !reuse {
+					cli, err = client.NewTftpClient(server)
+					cli.Blocksize = blocksize
+					if err != nil {
+						panic(err)
+					}
+
+				}
 				read := io.LimitReader(DataReader{}, int64(nbytes))
 				nbytes, err := cli.PutFile(fmt.Sprintf("file%d-%d", thr, j), read)
 				if err != nil {
@@ -113,6 +135,7 @@ func main() {
 	filename := flag.String("file", "", "name of file to work with (for reads only)")
 	upload := flag.Int("upload", -1, "size of data for upload testing")
 	blocksize := flag.Int("blocksize", 512, "tftp blocksize")
+	reuseport := flag.Bool("reuseport", true, "whether or not to reuse the same ports")
 
 	flag.Parse()
 
@@ -121,8 +144,8 @@ func main() {
 	fmt.Printf("Testing Server: '%s'\n", *serv)
 
 	if *upload > 0 {
-		benchWrites(*serv, *nthreads, *nloops, *upload, *blocksize)
+		benchWrites(*serv, *nthreads, *nloops, *upload, *blocksize, *reuseport)
 	} else {
-		benchReads(*serv, *filename, *nthreads, *nloops, *blocksize)
+		benchReads(*serv, *filename, *nthreads, *nloops, *blocksize, *reuseport)
 	}
 }
